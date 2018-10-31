967fe2a51a1b375858cb8dd30034004a422842aa
yujingtian <942843614@qq.com>
2018-10-31 21:21:59 +0800

    server

--- server.js
+++ server.js
@@ -6,18 +6,28 @@ app.use(express.static(__dirname,{index:"indexs.html"}))   //设置静态文件
 const server = require("http").createServer(app)
 const io = require("socket.io")(server)
 
-
-app.listen(3000,function(){
-    console.log("开始监听3000端口")
-})
-
-const Server = require("ws").Server;
-
-const ws = new Server({port:9999})
-
-ws.on('connection',function(socket){
-    socket.on("message", function (msg){
+io.on("connection",function(socket){
+    socket.send("青花瓷")
+    socket.on("message", function (msg) {
         console.log(msg);
         socket.send(`这是服务器对你说的话：${msg}`)
     })
 })
+
+server.listen(3000,function(){
+    console.log("开始监听3000端口")
+})
+// app.listen(3000,function(){
+//     console.log("开始监听3000端口")
+// })
+
+// const Server = require("ws").Server;
+
+// const ws = new Server({port:9999})
+
+// ws.on('connection',function(socket){
+//     socket.on("message", function (msg){
+//         console.log(msg);
+//         socket.send(`这是服务器对你说的话：${msg}`)
+//     })
+// })
