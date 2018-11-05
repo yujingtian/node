48e47b0b785a0b39d58db35777ac260238dbe0b2
yujingtian <yujingtian@zafh.com.cn>
2018-11-05 11:57:40 +0800

    。。

--- client.js
+++ client.js
@@ -1,10 +1,79 @@
-let ws = new WebSocket('ws:localhost:9999')
+const socket = io("/")
+socket.on("connect", () => {
+    console.log("连接成功")
+    socket.emit('getHistory')
+})
 
-ws.onopen = function(){
-    ws.send("不错哦，已经连接上了")
+
+//回车发送消息的方法
+function enterSend(e){
+    let code = event.keyCode;
+    if(code === 13) send();
 }
 
-ws.onmessage = function(res){
-    console.log(res)
-    console.log(res.data)
+input.onkeydown = function(e){
+    enterSend(e)
 }
+
+function join(room) {
+    socket.emit('join', room)
+}
+socket.on('joined',room =>{
+    document.getElementById(`join-${room}`).style.display = 'none'
+    document.getElementById(`leave-${room}`).style.display = 'inline-block'
+})
+
+function leave(room){
+    socket.emit('leave',room)
+}
+
+socket.on('leaved', room => {
+    document.getElementById(`join-${room}`).style.display = 'inline-block'
+    document.getElementById(`leave-${room}`).style.display = 'none'
+})
+
+socket.on("message", (data) => {
+    let li = document.createElement("li");
+    li.className = 'list-group-item';
+    li.innerHTML = `
+        <p style="color:#ccc">
+            <span class="user"  style="${data.color}">${data.user}</span>
+            ${data.createAt}
+        </p>
+        <p class="content" style="background:${data.color}">
+            ${data.content}
+        </p>
+    `
+    list.appendChild(li)
+    list.scrollTop = list.scrollHeight
+})
+socket.on('history', history => {
+    // history拿到的是一个数组，所以用map映射成新数组，然后再join一下连接拼成字符串
+       let html = history.map(data => {
+               return `<li class="list-group-item">
+            <p style="color: #ccc;"><span class="user" style="color:${data.color}">${data.user} </span>${data.createAt}</p>
+            <p class="content" style="background-color: ${data.color}">${data.content}</p>
+        </li>`;
+       }).join('');
+       list.innerHTML = html + '<li style="margin: 16px 0;text-align: center">以上是历史消息</li>';
+    // 将聊天区域的滚动条设置到最新内容的位置
+       list.scrollTop = list.scrollHeight;
+});
+
+
+function privateChat(e){
+    let target = event.target;
+    let user = target.innerHTML;
+    if(target.className == 'user'){
+        input.value = `@${user}`
+    }
+}
+
+list.onclick = function(e){
+    privateChat(e
+        );
+}
+
+// socket.on("disconnect", () => {
+//     console.log("断开链接成功")
+// })
--- server.js
+++ server.js
@@ -6,11 +6,138 @@ app.use(express.static(__dirname,{index:"indexs.html"}))   //设置静态文件
 const server = require("http").createServer(app)
 const io = require("socket.io")(server)
 
-io.on("connection",function(socket){
-    socket.send("青花瓷")
-    socket.on("message", function (msg) {
-        console.log(msg);
-        socket.send(`这是服务器对你说的话：${msg}`)
+
+const SYSTEM = '系统';
+let socketObj = {};
+let userColor = ['#00a1f4', '#0cc', '#f44336', '#795548', '#e91e63', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#ffc107', '#607d8b', '#ff9800', '#ff5722']
+let mysocket = {}
+let msgHistory = [];
+
+
+function shuffle(arr){
+    let len = arr.length,random;
+    while(0 !== len)
+    {
+        random = (Math.random() * len--)>>>0;
+        [arr[len],arr[random]] = [arr[random],arr[len]];
+    }
+    return arr;
+}
+
+
+io.on("connection",function(socket){   
+    let username;
+    let color;
+    let room = [];
+    mysocket[socket.id] = socket;
+    socket.on('message',function(msg){
+        if (username) {
+            //正则判断消息是否为私聊专属
+            let private = msg.match(/@([^ ]+)(.+)/);
+            if (private) {
+                //私聊的用户，正则匹配的第一个分组
+                let toUser = private[1];
+                //私聊的内容，正则匹配的第二个分组
+                let content = private[2];
+                //从socketObj中获取用户的socket
+                let toSocket = socketObj[toUser];
+                if (toSocket) {
+                    //私聊的用户发消息
+                    toSocket.send({
+                        user:username,
+                        color,
+                        content,
+                        createAt:new Date().toLocaleDateString()
+                    })
+                }
+            }
+            else{
+                if(rooms.length)
+                {
+                    let socketJson = {};
+                    rooms.forEach(room =>{
+                        // 取得进入房间内所对应的所有sockets的hash值，它便是拿到的socket.id
+                        let roomSockets = io.sockets.adapter.rooms[room].sockets;
+                        Object.keys(roomSockets).forEach(socketId => {
+                            console.log('socketId',socketId)
+                            if(!socketJson[socketId])
+                            {
+                                socketJson[socketId] = 1;
+                            }
+                        })
+                    })
+                    Object.keys(socketJson).forEach(socketId => {
+                        mySocket[socketId].emit("message",{
+                            user:message,
+                            color,
+                            content:msg,
+                            createAt: new Date().toLocaleString()
+                        })
+                    })
+                }
+                else
+                {
+                    io.emit('message', {
+                        user: username,
+                        color,
+                        content: msg,
+                        createAt: new Date().toLocaleString()
+                    });
+                    msgHistory.push({
+                        user:username,
+                        color,
+                        content:msg,
+                        createAt:new Date().toLocaleDateString()
+                    })
+                }
+            }
+        }
+        else {
+            username = msg;
+            color = shuffle(userColor)[0];
+            socket.broadcast.emit('message', {
+                user: SYSTEM,
+                color,
+                content: `${username}加入聊天`,
+                createAt: new Date().toLocaleDateString()
+            })
+            socketObj[username] = socket;
+        }
+    })
+    socket.on("getHistory",()=>{
+        if(msgHistory.length)
+        {
+            let history = msgHistory.slice(msgHistory.length - 20)
+            socket.emit("history",history)
+        }
+    })
+    socket.on("join",function(room){
+        if(username && rooms.indexOf(room) === -1)
+        {
+            socket.join(room)
+            room.push(room);
+            socket.emit('joined',room)
+            socket.send({
+                user:SYSTEM,
+                color,
+                content:`你已加入${room}战队`,
+                createAt:new Date().toLocaleDateString()
+            })
+        }
+    })
+    socket.on("leave", function (room) {
+        let index = rooms.indexOf(room);
+        if (index == -1) {
+            socket.leave(room)
+            room.splice(index,1);
+            socket.emit('joined', room)
+            socket.send({
+                user: SYSTEM,
+                color,
+                content: `你已离开${room}战队`,
+                createAt: new Date().toLocaleDateString()
+            })
+        }
     })
 })
 
